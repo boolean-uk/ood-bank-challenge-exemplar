29b7e0cca216e7b4b44946fc7f68888d1a523f9d
Carlo M <carlo992@gmail.com>
2022-01-23 11:53:09 +0100

    Added unit tests for transaction with Jasmine

---
+++ src/transcation.js
@@ -0,0 +1,42 @@
+
+
+class Transaction {
+    constructor(date, amount, type) {
+        this.date = date === undefined ? new Date() : date;
+        this.amount = (amount === undefined || amount <= 0) ? 0 : amount;
+        this.type = (type !== 'credit' && type !== 'debit') ? '' : type;
+    }
+
+    isValid() {
+        let validTransaction = true;
+        if(this.type !== "credit" && this.type !== "debit") {
+            validTransaction = false;
+        }
+        if(isNaN(this.date)) {
+            validTransaction = false;
+        }
+        if(this.amount <= 0) {
+            validTransaction = false;
+        }
+        return validTransaction;
+    }
+
+    getAmount() {
+        return this.amount;
+    }
+
+    getDate() {
+        return this.date;
+    }
+
+    getDateString() {
+        return this.date.toLocaleDateString();
+    }
+
+    getType() {
+        return this.type
+    }
+
+};
+
+module.exports = Transaction;

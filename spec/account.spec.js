8c296374d82d26c65d6e1c8e02aa38d5b541ff11
Carlo M <carlo992@gmail.com>
2022-01-25 11:57:28 +0100

    Implemented withdrawal

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -1,17 +1,25 @@
 const Account = require('./../src/account')
 
 
-describe("Acocunt class", function() {
+describe("Account class", function() {
   it("should accept valid deposit", () => {
     let account = new Account()
     let depositAmount = 100;
 
+    // check starting balance
+    let currentBalance = account.getBalance()
+    expect(currentBalance).toEqual(0)
+    
     let transaction = account.deposit(depositAmount)
 
     expect(transaction).not.toBeNull()
     expect(transaction.amount).toEqual(depositAmount)
     expect(transaction.type).toEqual('credit')
     expect(transaction.date.toLocaleDateString()).toBe(new Date().toLocaleDateString());
+
+    currentBalance = account.getBalance()
+    expect(currentBalance).toEqual(depositAmount)
+
   })
 
   it("should not accept a negative or zero deposit", () => {
@@ -19,5 +27,41 @@ describe("Acocunt class", function() {
 
     expect(account.deposit(-1)).toBeNull()
     expect(account.deposit(0)).toBeNull()
+  })
+
+  it("should accept valid withdrawal if enough cash", () => {
+    const startingBalance = 70
+    const withdrawAmount = 60;
+    const account = new Account(startingBalance)
+
+    // check starting balance
+    let currentBalance = account.getBalance()
+    expect(currentBalance).toEqual(startingBalance)
+
+    // perform 1 valid withdrawal
+    let transaction = account.withdraw(withdrawAmount)
+
+    expect(transaction).not.toBeNull()
+    expect(transaction.amount).toEqual(withdrawAmount)
+    expect(transaction.type).toEqual('debit')
+    expect(transaction.date.toLocaleDateString()).toBe(new Date().toLocaleDateString());
+
+    currentBalance = account.getBalance()
+    expect(currentBalance).toEqual(startingBalance - withdrawAmount)
+  })
+
+  it("should not accept a negative or zero withdrawal", () => {
+    let startingBalance = 100
+    let account = new Account(startingBalance)
+
+    expect(account.withdraw(-1)).toBeNull()
+    expect(account.withdraw(0)).toBeNull()
+  })
+
+  it("should not withdraw if not enough balance", () => {
+    let startingBalance = 100
+    let account = new Account(startingBalance)
+
+    expect(account.withdraw(2000)).toBeNull()
   })
 })
--- src/account.js
+++ src/account.js
@@ -1,16 +1,46 @@
 const Transaction = require("./transcation");
 
 class Account {
-  constructor() {
+  constructor(startingBalance) {
+    this.startingBalance = startingBalance === undefined ? 0 : startingBalance
     this.transactions = []
+  }
+
+  getBalance() {
+    let balance = this.startingBalance
+    for(var t = 0; t < this.transactions.length; t++) {
+      if(this.transactions[t].type === 'credit') {
+        balance += this.transactions[t].amount
+      }
+      else if(this.transactions[t].type === 'debit') {
+        balance -= this.transactions[t].amount
+      }
+    }
+    return balance
   }
 
   deposit(amount) {
     let transaction = null;
     if(amount > 0) {
       transaction = new Transaction(new Date(), amount, 'credit')
+      this.transactions.push(transaction)
     }
     return transaction;
+  }
+
+  canWidthdraw(amount) {
+    const currentBalance = this.getBalance()
+    const newBalance = currentBalance - amount
+    return (newBalance >= 0)
+  }
+
+  withdraw(amount) {
+    let transaction = null
+    if(amount > 0 && this.canWidthdraw(amount)) {
+      transaction = new Transaction(new Date(), amount, 'debit')
+      this.transactions.push(transaction)
+    }
+    return transaction
   }
 }
 

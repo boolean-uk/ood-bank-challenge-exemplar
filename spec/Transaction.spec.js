6d082f2a516319f8e5767f6d88b06f7f27633557
dearshrewdwit <dearshrewdwit@gmail.com>
2022-01-27 10:53:56 +0000

    mock date parser

--- spec/Transaction.spec.js
+++ spec/Transaction.spec.js
@@ -4,36 +4,45 @@ describe("Transaction", () => {
   let transactionCredit, transactionDebit, mockParser
 
   beforeEach(() => {
-    mockParser = () => "date"
+    mockParser = jasmine.createSpy('mockParser')
     transactionCredit = new Transaction({type: "credit", amount: 1000, date: "10-01-2012"}, mockParser)
     transactionDebit = new Transaction({type: "debit", amount: 200, date: "11-01-2012"}, mockParser)
   })
-  describe("#getDate()", () => {
-    it("returns string version for date", () => {
-      expect(transactionCredit.getDate()).toEqual("date")
-    })
 
-    describe("credit transactions", () => {
-      it("return credit value", () => {
-        expect(transactionCredit.credit()).toEqual("1000.00")
-      })
-      it("return no debit value", () => {
-        expect(transactionCredit.debit()).toEqual("")
-      })
-      it("return positive value", () => {
-        expect(transactionCredit.value()).toEqual(1000)
-      })
+  describe(".constructor()", () => {
+    it("calls date parser function", () => {
+      new Transaction({}, mockParser)
+      expect(mockParser).toHaveBeenCalled()
     })
-    describe("debit transactions", () => {
-      it("return no credit value", () => {
-        expect(transactionDebit.credit()).toEqual("")
-      })
-      it("return debit value", () => {
-        expect(transactionDebit.debit()).toEqual("200.00")
-      })
-      it("return negative value", () => {
-        expect(transactionDebit.value()).toEqual(-200)
-      })
+  })
+
+  describe("#getDate()", () => {
+    it("returns parser function return value", () => {
+      const transaction = new Transaction({}, () => "date")
+      expect(transaction.getDate()).toEqual("date")
+    })
+  })
+
+  describe("credit transactions", () => {
+    it("return credit value", () => {
+      expect(transactionCredit.credit()).toEqual("1000.00")
+    })
+    it("return no debit value", () => {
+      expect(transactionCredit.debit()).toEqual("")
+    })
+    it("return positive value", () => {
+      expect(transactionCredit.value()).toEqual(1000)
+    })
+  })
+  describe("debit transactions", () => {
+    it("return no credit value", () => {
+      expect(transactionDebit.credit()).toEqual("")
+    })
+    it("return debit value", () => {
+      expect(transactionDebit.debit()).toEqual("200.00")
+    })
+    it("return negative value", () => {
+      expect(transactionDebit.value()).toEqual(-200)
     })
   })
 })

98139b4aad785400e78af399ed2284299c0d5c20
steve <steve.huckle@gmail.com>
2022-06-21 09:29:12 +0100

    tidy

--- src/dateFormat.js
+++
@@ -1,9 +0,0 @@
-const parseDateDMY = (dateString) => {
-  const parts = dateString.split('-')
-  parts[1] -= 1 // months are 0-indexed
-  return new Date(...parts.reverse())
-}
-
-module.exports = {
-  parseDateDMY: parseDateDMY
-}
